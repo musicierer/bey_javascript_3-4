59d8be3c26a022ab284dd9b6ce55a913dff85b66
musicierer <musicierer@gmail.com>
2017-02-19 18:07:17 +0200

    homework 3-4

---
+++ ДЗ 3-4/script.js
@@ -0,0 +1,167 @@
+var element = document.createElement('div');
+	element.classList.add('container');
+document.body.appendChild(element);
+
+var row = document.createElement('div');
+	row.classList.add('row');
+var container = document.querySelector('.container');
+container.appendChild(row);
+
+var h1 = document.createElement('h1');
+h1.classList.add('col-lg-12');
+var row = document.querySelector('.row');
+row.appendChild(h1);
+h1.innerHTML = 'Тест по программированию';
+h1.style.fontSize = '24px';
+h1.style.textAlign = 'center';
+h1.style.marginBottom = '20px';
+
+var firstCheckbox = document.createElement('div');
+	firstCheckbox.classList.add('firstCheckbox');
+	firstCheckbox.classList.add('col-lg-6');
+var row = document.querySelector('.row');
+row.appendChild(firstCheckbox);
+
+var p = document.createElement('p');
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(p);
+p.innerHTML = '1. Вопрос №1';
+p.style.fontSize = '20px';
+p.style.marginBottom = '10px';
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №1 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №2 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №3';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var p = document.createElement('p');
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(p);
+p.innerHTML = '2. Вопрос №2';
+p.style.fontSize = '20px';
+p.style.marginBottom = '10px';
+p.style.marginTop = '10px';
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №1 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №2 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №3 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var p = document.createElement('p');
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(p);
+p.innerHTML = '3. Вопрос №3';
+p.style.fontSize = '20px';
+p.style.marginBottom = '10px';
+p.style.marginTop = '10px';
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №1 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №2 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'checkbox';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.marginLeft = '20px';
+
+var label = document.createElement('label');
+label.htmlFor = input;
+label.innerHTML = 'Вариант ответа №3 <br>';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(label);
+
+var input = document.createElement('input');
+input.type = 'button';
+input.value = 'Проверить мои результаты';
+var firstCheckbox = document.querySelector('.firstCheckbox');
+firstCheckbox.appendChild(input);
+input.style.width = '327px';
+input.style.height = '60px';
+input.style.border = '2px solid #000205';
+input.style.backgroundColor = '#cfe2f3';
+input.style.fontSize = '18px';
+input.style.marginTop = '50px';
+input.style.marginLeft = '430px';
